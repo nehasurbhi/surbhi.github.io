452419bcd46ea5fbb522ae24ce12d360ff2b1f1c
nehasurbhi <75774920+nehasurbhi@users.noreply.github.com>
2021-04-04 00:40:39 +0530

    Photography

---
+++ countdown.js
@@ -0,0 +1,36 @@
+$.fn.jQuerySimpleCounter = function( options ) {
+    var settings = $.extend({
+        start:  0,
+        end:    100,
+        easing: 'swing',
+        duration: 700,
+        complete: ''
+    }, options );
+
+    var thisElement = $(this);
+
+    $({count: settings.start}).animate({count: settings.end}, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function() {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+        },
+        complete: settings.complete
+    });
+};
+
+
+$('#number1').jQuerySimpleCounter({end: 18,duration: 7000});
+$('#number2').jQuerySimpleCounter({end: 59,duration: 7000});
+$('#number3').jQuerySimpleCounter({end: 359,duration: 5000});
+$('#number4').jQuerySimpleCounter({end: 246,duration: 4500});
+
+
+
+  /* AUTHOR LINK */
+ $('.about-me-img').hover(function(){
+        $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+    }, function(){
+        $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+        });
